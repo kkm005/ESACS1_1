3931bb326f1fd2916346c835692fb0a38667b51b
kkm005 <33860679+kkm005@users.noreply.github.com>
2017-11-26 20:39:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,46 +1,33 @@
-case "t_c":
-    // กำหนด action 4 ปุ่ม 4 ประเภท
-    $actionBuilder = array(
-        new MessageTemplateActionBuilder(
-            'Message Template',// ข้อความแสดงในปุ่ม
-            'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-        ),
-        new UriTemplateActionBuilder(
-            'Uri Template', // ข้อความแสดงในปุ่ม
-            'https://www.ninenik.com'
-        ),
-        new PostbackTemplateActionBuilder(
-            'Postback', // ข้อความแสดงในปุ่ม
-            http_build_query(array(
-                'action'=>'buy',
- 
-                'item'=>100
-            )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-            'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-        ),      
-    );
-    $replyData = new TemplateMessageBuilder('Carousel',
-        new CarouselTemplateBuilder(
-            array(
-                new CarouselColumnTemplateBuilder(
-                    'Title Carousel',
-                    'Description Carousel',
-                    'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                    $actionBuilder
-                ),
-                new CarouselColumnTemplateBuilder(
-                    'Title Carousel',
-                    'Description Carousel',
-                    'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                    $actionBuilder
-                ),
-                new CarouselColumnTemplateBuilder(
-                    'Title Carousel',
-                    'Description Carousel',
-                    'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/700',
-                    $actionBuilder
-                ),                                          
-            )
-        )
-    );
-    break;  
+{
+  "type": "template",
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://example.com/bot/images/item1.jpg",
+            "action": {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=111"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item2.jpg",
+            "action": {
+              "type": "message",
+              "label": "Yes",
+              "text": "yes"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item3.jpg",
+            "action": {
+              "type": "uri",
+              "label": "View detail",
+              "uri": "http://example.com/page/222"
+            }
+          }
+      ]
+  }
+}
