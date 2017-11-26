06a5035826ba5464a8a9bd667c2a52a14bd1baf1
kkm005 <33860679+kkm005@users.noreply.github.com>
2017-11-26 22:54:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,3 @@
 curl -X GET \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-H 'Authorization: Bearer {e7j8LZupeYDsw1/VE9+xrQu6QxMvmYSwO2vSNcl3ttHhyjc8LS7XJDr4qTqDRsIhaefBDgXdPURZtLiGp+xF9dEIAqSYV17mjIbo/6XFvYQNDCeNvMi034wWBKU7cPUGALZ3vpCq+zQ3b9gFnNVhZAdB04t89/1O/w1cDnyilFU=}' \
 https://api.line.me/v1/oauth/verify
